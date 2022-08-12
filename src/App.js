1f73cbcf613ddad6c5060e31cdb27b85f08d1de8
Udaf999999999 <udaf999999999@yandex.ru>
2022-08-12 20:34:43 +0700

    Lesson_2

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [messageList, setmessageList] = useState([]);
+  const [value, setValue] = useState('');
+
+
+  const handleClick = (event) => {
+    setmessageList([...messageList, { author: "author1", text: value, date: new Date }]);
+  }
+
+  const handleChange = (event) => {
+    setValue(event.target.value);
+  }
+
+  useEffect(() => {
+    if (messageList.length !== 0 && messageList[messageList.length - 1].author != "robot1") {
+      setTimeout(() => {
+        setmessageList([...messageList, { author: "robot1", text: messageList[messageList.length - 1].author + " send message.", date: new Date }])
+      }, 1500);
+
+    }
+  }, [messageList]);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <span>message: </span>
+      <input type="text" value={value} onChange={handleChange} />
+      <button className="btn" onClick={handleClick}>Send Message</button>
+
+      {messageList.map((element) => (
+        <div key={element.author + element.date.getTime()}>
+          {element.author + " (" + element.date.toTimeString().split(' ')[0] + "): " + element.text}
+        </div>))
+      }
     </div>
-  );
+  )
 }
 
 export default App;
