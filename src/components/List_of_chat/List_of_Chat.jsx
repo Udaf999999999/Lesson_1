5c5650a7bce907d3cfffbfd836a415188cbec293
Udaf999999999 <udaf999999999@yandex.ru>
2022-08-16 22:00:26 +0700

    5_almost_almost

--- src/components/List_of_chat/List_of_Chat.jsx
+++ src/components/List_of_chat/List_of_Chat.jsx
@@ -36,19 +36,3 @@ export default function List_of_Chat() {
 
   );
 }
-/*<Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'background.paper' }}>
-          <List component="nav" aria-label="secondary mailbox folder">
-            <ListItemButton
-              selected={selectedIndex === 2}
-              onClick={(event) => handleListItemClick(event, 2)}
-            >
-              <ListItemText primary="Trash" />
-            </ListItemButton>
-            <ListItemButton
-              selected={selectedIndex === 3}
-              onClick={(event) => handleListItemClick(event, 3)}
-            >
-              <ListItemText primary="Spam" />
-            </ListItemButton>
-          </List>
-    </Box>*/
